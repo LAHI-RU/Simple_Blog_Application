d8c1d0deab099a5946199038a1e5bf52a136b76a
Lahiru Dhananjaya <lahiiru.dananjaya@gmail.com>
2024-11-02 11:25:41 +0530

    update

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 
@@ -10,3 +12,12 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running one port ${PORT}`));
+
+const mongoose = require("mongoose");
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log("MongoDB connection error:", err));
